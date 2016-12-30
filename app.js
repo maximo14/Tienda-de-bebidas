82785d1e3658fbd548bf5efed205a570312d7f17
Maximo Fiora <maxi.fiora14@gmail.com>
2016-12-30 17:24:50 -0300

    se agrego conexion simple a mongo db

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
 var app = express();
 
+mongoose.connect("mongodb://localhost/tienda_bebidas");
 
+var userSchemaJSON = {
+    email: String,
+    password: String
+};
+
+var user_schema = Schema(userSchemaJSON);
+var User = mongoose.model("User",user_schema);
+
+// Use native promises -- Nose porque es esto pero ahce que ande
+mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -11,16 +25,18 @@ app.set("view engine","pug");
 
 app.get("/", (req, res) => {
     res.render("index");   
-})
+});
 
 app.get("/login",(req,res)=>{
     res.render("login");
 });
 
 app.post("/users",(req,res)=>{
-    console.log("Email: "+req.body.email);
-    console.log("Constraseña: "+req.body.password);
-    res.send("resibimos tus datos");
+    var user = new User({email: req.body.email, password: req.body.password});
+
+    user.save(function(){
+        res.send("Guardamos tus datos");
+    });   
 });
 
 app.listen(3000);
