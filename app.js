567f0c14bd0446f39423f65c5724241cc1f204e7
Maximo Fiora <maxi.fiora14@gmail.com>
2016-12-30 16:35:35 -0300

    se agrego la funcion de login

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
+
+
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","pug");
 
 app.get("/", (req, res) => {
     res.render("index");   
 })
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.post("/users",(req,res)=>{
+    console.log("Email: "+req.body.email);
+    console.log("Constraseña: "+req.body.password);
+    res.send("resibimos tus datos");
+});
+
 app.listen(3000);
