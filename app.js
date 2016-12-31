44cc668cb29880f1c12055ff084c4afc27702a33
Maximo Fiora <maxi.fiora14@gmail.com>
2016-12-31 15:30:22 -0300

    se agregaron comentarios a los archivos

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+//cargamos los fremaworks, el 'require' es como el import en java.
 var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
 
+// uso express
 var app = express();
 
-
+//sirvo los archivos de la carpeta public para poder hacer uso de ellos, (el css, js, etc)
 app.use(express.static('public'));
+
+//funcion del body parser para el manejo de JSON
 app.use(bodyParser.json());
+
+//hago que express use body-parser para poder acceder a los elementos del html
 app.use(bodyParser.urlencoded({extended: true}));
+
+//seteo el motor de plantillas, en este caso pug (antes de llamaba jade)
 app.set("view engine","pug");
 
+//se asigna la pagina index al la pag principal
 app.get("/", (req, res) => {
     res.render("index");   
 });
+
 
 app.get("/login",(req,res)=>{
     res.render("login");
@@ -26,4 +36,5 @@ app.post("/users",(req,res)=>{
     });   
 });
 
+//se inicia la aplicacion para que escuche en el puerto 3000
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ mongoose.connect("mongodb://localhost/tienda_bebidas");
 
 // Use native promises -- Nose porque es esto pero ahce que ande
 mongoose.Promise = global.Promise;
+
+//se pueden agregar validaciones a los equemas.. ver doc Mongoose validation
+//se puede usar las que vienen con moonguse o hacer validaciones personalizadas
 var user_schema = new Schema({
     email: String,
     password: String
@@ -11,4 +14,5 @@ var user_schema = new Schema({
 
 var User = mongoose.model("User",user_schema);
 
+//exporto el modulo para que pueda ser accesido desde cualquier parte
 module.exports.User = User;
