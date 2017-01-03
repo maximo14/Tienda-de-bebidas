568622a30338cc3dde5b987705890dae7012bedd
Maximo Fiora <maxi.fiora14@gmail.com>
2017-01-03 18:12:13 -0300

    se mejoro el manejo de rutas modular

--- app.js
+++ app.js
@@ -37,46 +37,6 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-//app.get("/login", (req, res) => {
-//    res.render("login");
-//});
-
-app.post("/logear",(req, res)=>{
-    User.findOne({username:req.body.username, password: req.body.password},(err,docs)=>{
-        if (err){
-            console.log(err);
-        }else{
-            console.log(docs);
-            req.session.usuario = docs;
-            res.send("Usuario logiado");
-        }        
-    });
-});
-
-
-//app.get("/register", (req, res) => {
-//    res.render("register");
-//});
-
-//se encarga de registar a los usuarios
-app.post("/users", (req, res) => {
-    var user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        password_confirmation: req.body.passwordrepit
-    });
-
-    //guardado de usuarios implementando promesas
-    user.save().then(us=>{       
-        res.send("guardamos el usuario correctamente");
-    },err =>{
-        if(err){
-            console.log(String(err));
-        }
-    });
-});
-
 
 //rutas modulares
 app.use("/users",router_users);
--- controllers/ctr_users.js
+++ controllers/ctr_users.js
@@ -1,12 +1,43 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../models/user").User;
 
 router.get("/login",(req,res)=>{
     res.render("users/login");
 });
 
+router.post("/login",(req, res)=>{
+    User.findOne({username:req.body.username, password: req.body.password},(err,docs)=>{
+        if (err){
+            console.log(err);
+        }else{
+            console.log(docs);
+            req.session.usuario = docs;
+            res.send("Usuario logiado");
+        }        
+    });
+});
+
 router.get("/register", (req, res) => {
     res.render("users/register");
+});
+
+router.post("/register", (req, res) => {
+    var user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        password_confirmation: req.body.passwordrepit
+    });
+
+    //guardado de usuarios implementando promesas
+    user.save().then(us=>{       
+        res.send("guardamos el usuario correctamente");
+    },err =>{
+        if(err){
+            console.log(String(err));
+        }
+    });
 });
 
 module.exports = router;
